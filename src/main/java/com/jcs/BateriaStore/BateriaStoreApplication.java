1836a3f97c01dcdae0bac0e3cb94710c7b31ee65
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-03-02 13:02:32 -0300

    Criado Projeto - Packages and resources

---
+++ src/main/java/com/jcs/BateriaStore/BateriaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.BateriaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BateriaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BateriaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcs/BateriaStore/BateriaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.BateriaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BateriaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
