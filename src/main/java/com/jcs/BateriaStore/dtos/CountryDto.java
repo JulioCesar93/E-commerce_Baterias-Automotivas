3692238edb872b6c92effcfb0d9016f5b793dff5
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-03-19 20:44:17 -0300

    CountryDTO

--- src/main/java/com/jcs/BateriaStore/dtos/CountryDto.java
+++ src/main/java/com/jcs/BateriaStore/dtos/CountryDto.java
@@ -1,7 +1,17 @@
 package com.jcs.BateriaStore.dtos;
 
-public class CountryDto {
+import com.jcs.BateriaStore.entities.Country;
+
+import java.io.Serializable;
+
+public class CountryDto implements Serializable {
     private Long id;
     private String code;
     private String name;
+
+    public CountryDto (Country entity) {
+        id = entity.getId();
+        code = entity.getCode();
+        name = entity.getName();
+    }
 }
