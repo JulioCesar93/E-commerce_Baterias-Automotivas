53f16be4c48116c319b0a6e156acfc9d7b36fc9c
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-03-21 21:36:38 -0300

    Classe Order by Produto/Item

---
+++ src/main/java/com/jcs/BateriaStore/entities/Pgto/OrderProdPgto.java
@@ -0,0 +1,28 @@
+package com.jcs.BateriaStore.entities.Pgto;
+
+import com.jcs.BateriaStore.entities.Order;
+import com.jcs.BateriaStore.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderProdPgto implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
+
