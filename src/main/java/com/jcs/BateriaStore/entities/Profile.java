7055240cf19efc6ce1ca03fc7d64dad434eb1cd4
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-03-26 00:01:58 -0300

    @Annotations - Profile e State

--- src/main/java/com/jcs/BateriaStore/entities/Profile.java
+++ src/main/java/com/jcs/BateriaStore/entities/Profile.java
@@ -1,10 +1,15 @@
 package com.jcs.BateriaStore.entities;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_profile")
 public class Profile implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
--- src/main/java/com/jcs/BateriaStore/entities/State.java
+++ src/main/java/com/jcs/BateriaStore/entities/State.java
@@ -1,4 +1,17 @@
 package com.jcs.BateriaStore.entities;
 
-public class State {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "state")
+public class State implements Serializable {
+
+    @Id
+    private Long id;
+    private String name;
+    private Country country;
 }
