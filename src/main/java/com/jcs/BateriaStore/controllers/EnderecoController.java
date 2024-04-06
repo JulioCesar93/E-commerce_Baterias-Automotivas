11608021f6555ebebb9428c17aecdefe4d207134
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-04-06 14:39:14 -0300

    Implementação findAll - ClassPackage Controllers

--- src/main/java/com/jcs/BateriaStore/controllers/EnderecoController.java
+++ src/main/java/com/jcs/BateriaStore/controllers/EnderecoController.java
@@ -6,7 +6,8 @@ import com.jcs.BateriaStore.services.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,7 +26,7 @@ public class EnderecoController {
         return ResponseEntity.ok().body(list);
     }
 
-    //busca endereco pelo id do usurio => /find?userId=2
+    //busca endereco pelo id do usurio => /find?userId=93
     @GetMapping("/find")
     public ResponseEntity<List<EnderecoDto>> getByUserId(@RequestParam("userId") Long userId) {
         List<EnderecoDto> list = service.getByUserId(userId);
@@ -38,6 +39,24 @@ public class EnderecoController {
         EnderecoDto dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
-}
 
-//Add Busca por id: Get, Put, Post e Delete
+    @PostMapping
+    public ResponseEntity<EnderecoDto> insert(@RequestBody EnderecoDto dto) {
+        EnderecoDto newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EnderecoDto> update(@PathVariable Long id, @RequestBody EnderecoDto dto) {
+        EnderecoDto newDto = service.update(id, dto);
+        return ResponseEntity.ok().body(newDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/jcs/BateriaStore/entities/Endereco.java
+++ src/main/java/com/jcs/BateriaStore/entities/Endereco.java
@@ -1,9 +1,6 @@
 package com.jcs.BateriaStore.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,8 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class Endereco implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String cep;
     private String logradouro;
@@ -24,7 +23,6 @@ public class Endereco implements Serializable {
     private String bairro;
     private String localidade;
     private String uf;
-    //private User user;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
