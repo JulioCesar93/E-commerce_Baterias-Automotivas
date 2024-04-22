6fc3acb73fd345534a593ae36f5516765f3e8c3e
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-04-21 21:20:41 -0300

    Anotações e Métodos Jwt em Config: AuthorizationServerConfig e LocalDateConfig

--- src/main/java/com/jcs/BateriaStore/config/AuthorizationServerConfig.java
+++ src/main/java/com/jcs/BateriaStore/config/AuthorizationServerConfig.java
@@ -1,34 +1,73 @@
 package com.jcs.BateriaStore.config;
 
 import com.jcs.BateriaStore.components.JwtTokenIntermediary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
+@EnableAuthorizationServer
 @Configuration
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Value("${security.oauth2.client.client-id}")
     private String clientCod;
+
+    @Value("${security.oauth2.client.client-secret}")
     private String clientSecret;
+
+    @Value("${jwt.time}")
     private Integer jwtTime;
+
+    @Autowired
     private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
     private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
     private JwtTokenStore tokenStoreJwt;
+
+    @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
     private JwtTokenIntermediary jwtTokenIntermediary;
 
+    @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 
+    @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("ecommerce-baterias")
+                .secret(passwordEncoder.encode("bateria034"))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(jwtTime);
     }
 
+    @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain chain = new TokenEnhancerChain();
+        chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, jwtTokenIntermediary));
+
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStoreJwt)
+                .accessTokenConverter(accessTokenConverter)
+                .tokenEnhancer(chain);
     }
 }
--- src/main/java/com/jcs/BateriaStore/config/LocalDateConfiguration.java
+++ src/main/java/com/jcs/BateriaStore/config/LocalDateConfiguration.java
@@ -1,14 +1,24 @@
 package com.jcs.BateriaStore.config;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Configuration;
 
+import java.time.format.DateTimeFormatter;
+
+@Configuration
 public class LocalDateConfiguration {
 
     private static final String datePattern = "dd/MM/yyyy";
     private static final String dateTimePattern = "dd/MM/yyyy HH:mm:ss";
 
     public Jackson2ObjectMapperBuilderCustomizer localDateCustomizer() {
-        return null;
-        // builder
+        return builder ->
+        {
+            builder.simpleDateFormat(dateTimePattern);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(datePattern)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimePattern)));
+        };
     }
 }
